6566cce9490a6eee87a0ee0737d7839b259a6769
zhanglinhua <903188706@qq.com>
2017-06-23 17:02:24 +0800

    third

--- app/src/main/java/com/example/zhang/pintugame/MainActivity.java
+++ app/src/main/java/com/example/zhang/pintugame/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.zhang.pintugame;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
@@ -30,13 +34,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mTime;
     private Handler mhandler=new Handler(this);
     private int  time=0;
+    private  boolean isgaming;
+
+    private Bitmap[] bitmaps=new Bitmap[9];//加入图片 切成9张
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBitmaps();
         init();
         randomintview();
+    }
+
+    private void initBitmaps() {
+        //加载完成的图片
+        Bitmap bitmapimag = BitmapFactory.decodeResource(getResources(), R.mipmap.wzry);
+        int width = bitmapimag.getWidth();
+        int height = bitmapimag.getHeight();//获得原图的宽高
+        int singlewidth=width/3;
+        int singleheight=height/3;
+        for (int i = 0; i <9 ; i++) {
+            /*0x0 0x1 0x2
+              1x0 1x1 1x2
+              2x0 2x1 2x2
+
+             */
+            Bitmap imag_slide = Bitmap.createBitmap(bitmapimag, (i % 3) * singlewidth, (i / 3) * singleheight, singlewidth, singleheight);
+            bitmaps[i]=imag_slide;
+
+
+        }
     }
 
     private void randomintview() {
@@ -59,8 +87,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             imageview.setScaleType(ImageView.ScaleType.FIT_XY);
 
 
-            imageview.setImageResource(pics[picIndex[i]]);
-            if (i==childCount-1){
+            imageview.setVisibility(View.VISIBLE);//全都可见 重置不设置有bug
+          //静态
+           // imageview.setImageResource(pics[picIndex[i]]);
+            //动态 将所有的静态换成动态
+             imageview.setImageBitmap(bitmaps[picIndex[i]]);
+            if (i==blankIndex){
                 imageview.setVisibility(View.INVISIBLE);
             }
         }
@@ -91,16 +123,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
         if(flag){
-            Toast.makeText(this, "你成功了,我很看好你，快打钱给我Q903188706", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "你成功了,时间"+time+"，快打钱给我Q903188706", Toast.LENGTH_SHORT).show();
             int childcount=mGroup.getChildCount();
             for(int i=0;i<childcount;i++){
                  View view =mGroup.getChildAt(i);
                 view.setVisibility(View.VISIBLE);
                 view.setClickable(false);
                 if(i==childcount-1){
-                    ((ImageView) view).setImageResource(pics[childcount-1]);
+             //静态     --
+                   // ((ImageView) view).setImageResource(pics[childcount-1]);
+              //动态
+                    ((ImageView)view).setImageBitmap(bitmaps[childcount-1]);
                 }
             }
+
+            isgaming=false;
+            //停止计时
+            mhandler.removeMessages(UP_DATA_TIME);
+           AlertDialog.Builder build= new AlertDialog.Builder(this).setMessage("你成功了,时间"+time+"，快打钱给我Q903188706").setTitle("过关")
+                   .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           Toast.makeText(MainActivity.this, "确认恭喜", Toast.LENGTH_SHORT).show();
+                       }
+                   })
+                   .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           Toast.makeText(MainActivity.this, "未来继续努力", Toast.LENGTH_SHORT).show();
+                       }
+                   })
+                   ;
+
+            build.create().show();
         }
 
     }
@@ -124,7 +179,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.begin:
-                gameStart();
+                if(!isgaming) {
+                    isgaming=true;
+                    mBegin.setText("重新开始");
+                    gameStart();
+                }
+                else{//重置游戏
+                    time=0;
+
+                    mhandler.removeMessages(UP_DATA_TIME);
+                    randomintview();
+                    gameStart();
+
+
+                }
                 break;
             default:
                 int position=mGroup.indexOfChild(v);//不知道点击的具体的view的ID  但是他的父控件知道
@@ -161,7 +229,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mGroup.getChildAt(position).setVisibility(View.INVISIBLE);
             ImageView image=(ImageView)mGroup.getChildAt(blankIndex);
             image.setVisibility(View.VISIBLE);
-            image.setImageResource(pics[picIndex[position]]);
+           //静态 --
+           // image.setImageResource(pics[picIndex[position]]);
+         //动态
+            image.setImageBitmap(bitmaps[picIndex[position]]);
             swap(position,blankIndex);
             blankIndex=position;
             isSuccess();
